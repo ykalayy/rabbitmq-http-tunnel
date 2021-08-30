8778456d025bdea85f74a3d079accbc6a2c4e9bf
KALAY <ykalay1997@gmail.com>
2021-08-30 23:38:04 +0300

    Added deRegisterNettyChannel function into NettyChannelTimeoutTask to remove channel from NettyChannelStore

--- src/main/java/com/ykalay/rabbitmqtunnel/core/netty/NettyChannelTimeoutTask.java
+++ src/main/java/com/ykalay/rabbitmqtunnel/core/netty/NettyChannelTimeoutTask.java
@@ -10,19 +10,25 @@ public class NettyChannelTimeoutTask implements Runnable {
     private final Channel channel;
     private final HttpAmqpTunnelTimeoutHandler<?> httpAmqpTunnelTimeoutHandler;
     private final AmqpMessage amqpMessage;
+    private final NettyChannelStore nettyChannelStore;
 
-    public NettyChannelTimeoutTask(Channel channel, HttpAmqpTunnelTimeoutHandler httpAmqpTunnelTimeoutHandler, AmqpMessage<?> amqpMessage) {
+    public NettyChannelTimeoutTask(Channel channel, HttpAmqpTunnelTimeoutHandler<?> httpAmqpTunnelTimeoutHandler, AmqpMessage<?> amqpMessage) {
         this.channel = channel;
         this.httpAmqpTunnelTimeoutHandler = httpAmqpTunnelTimeoutHandler;
         this.amqpMessage = amqpMessage;
+        this.nettyChannelStore = NettyChannelStore.getInstance();
     }
 
     @Override
     public void run() {
-        if(httpAmqpTunnelTimeoutHandler == null) {
-            BaseNettyHandler.sendResponseWithNoBody(channel, HttpResponseStatus.REQUEST_TIMEOUT);
-        } else {
-            httpAmqpTunnelTimeoutHandler.handleTimeout(amqpMessage, BaseNettyHandler.getURIFromAttr(channel));
+        try {
+            if(this.httpAmqpTunnelTimeoutHandler == null) {
+                BaseNettyHandler.sendResponseWithNoBody(channel, HttpResponseStatus.REQUEST_TIMEOUT);
+            } else {
+                this.httpAmqpTunnelTimeoutHandler.handleTimeout(amqpMessage, BaseNettyHandler.getURIFromAttr(channel));
+            }
+        } finally {
+            this.nettyChannelStore.deRegisterNettyChannel(amqpMessage.getUniqueMessageId());
         }
     }
 }
